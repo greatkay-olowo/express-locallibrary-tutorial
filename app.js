91ac301b826c994f963b570e7e0ce28895dfc83b
Kayode Alao <oluwakayode.alao@gmail.com>
2019-11-20 20:00:51 +0100

    add folder structure and schema

---
+++ app.js
@@ -0,0 +1,52 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//Set up mongoose connection
+const mongoDB =
+  'mongodb+srv://greatkay-olowo:OLUWAkayode19931@cluster0-b52eg.mongodb.net/test?retryWrites=true&w=majority';
+mongoose
+  .connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('Connected to Database'))
+  .catch(err => console.error('An error has occured', err));
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
